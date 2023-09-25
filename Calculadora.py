0f8cc382fa47c21b0a9db80264cf5ada82c5a23f
Matheus Yan <reismatheusyan83@gmail.com>
2023-09-25 14:10:43 -0300

    update

--- Calculadora.py
+++ Calculadora.py
@@ -1,26 +1,28 @@
-equacao=str(input('Qual sua equação?'))
-if equacao == 'Mais' or '+' or 'mais' :
-    n1=float(input('Qual seu primeiro numero?'))
-    n2=float(input('Qual seu segundo numero?'))
-    total=n1+n2
-    print('Seu resultado é {}'.format(total))
-elif equacao== 'Menos' or '-' or 'menos':
-    n1=float(input('Qual seu primeiro numero?'))
-    n2=float(input('Qual seu segundo numero?'))
-    total = n1-n2
-    print ('O resultado da subtração entre {} e {} é igual a:'.format (n1,n2), total)
-elif equacao == 'Divisão' or '/' or 'divisão' :
-    n1=float(input('Qual seu primeiro numero?'))
-    n2= float(input('Qual o segundo número ? '))
-    total=n1/n2
-    print('A divisão de {} por {} resulta em {:.3f}'. format(n1 , n2 , total ))
-elif equacao== 'Multiplicação' or '*' or 'multiplicação' :
-    n1=float(input('Qual seu primeiro numero?'))
-    n2=float(input('Qual seu segundo numero?'))
-    print('O produto do valor digitado é', n1*n2 )
-elif equacao == 'Exponenciação' or 'Exponencial' or '**' :
-    n1=float(input('Qual seu primeiro numero?'))
-    n2=float(input('Qual seu segundo numero?'))
-    print('{} elevado à potência de {} é igual a:{} '.format(n1,n2,(n1)**(n2)))
-else :
-    print('Opção inválida')
+def calcular_equacao(equacao):
+    # Validar a entrada do usuário
+    if equacao not in ["Mais", "+", "mais", "Menos", "-", "menos", "Divisão", "/", "divisão", "Multiplicação", "*", "multiplicação", "Exponenciação", "**"]:
+        raise ValueError("Opção inválida")
+
+    # Obter os números da entrada do usuário
+    n1 = float(input("Qual seu primeiro numero?"))
+    n2 = float(input("Qual seu segundo numero?"))
+
+    # Calcular o resultado da equação
+    if equacao == "Mais" or equacao == "+" or equacao == "mais":
+        total = n1 + n2
+    elif equacao == "Menos" or equacao == "-" or equacao == "menos":
+        total = n1 - n2
+    elif equacao == "Divisão" or equacao == "/" or equacao == "divisão":
+        total = n1 / n2
+    elif equacao == "Multiplicação" or equacao == "*" or equacao == "multiplicação":
+        total = n1 * n2
+    elif equacao == "Exponenciação" or equacao == "**":
+        total = n1 ** n2
+
+    # Retornar o resultado
+    return total
+
+
+equacao = input("Qual sua equação? ")
+resultado = calcular_equacao(equacao)
+print("O resultado é", resultado)
