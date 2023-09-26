b618ef9598a8ed7060ab27e2143eeac342a06083
Gustax0 <gustavohenriquegomesdias@gmail.com>
2023-09-26 19:54:02 -0300

    AGORA DEU CERTO HAHAHAH(quase)

--- interfaceTeste.py
+++ interfaceTeste.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import *
+from PIL import Image, ImageTk
 
 root = tk.Tk()
 
@@ -7,21 +8,28 @@ class Calculadora:
     def __init__(self):
         self.root = root
         self.interface()
-        self.exibirNum(30.5, 18.8, 30.5 + 257.5, 18.8 + 96, radius=15, fill="#DE2861")  # Define a cor de fundo como a cor desejada
+        self.exibirNum()
+        self.exibirImagem()
 
     def interface(self):
         root.title('Calculadora')
         root.geometry('320x480')
         root.resizable(False, False)
-        root.configure(background="#DE2861")
 
-    def exibirNum(self, x1, y1, x2, y2, radius=0, **kwargs):
+    def exibirNum(self):
         self.valor_inteiro = 0
-        self.label = Label(root, text=str(self.valor_inteiro), font=("ArchivoBlack", 38), fg="#FFFFFF", bg="#AA125F")  # Defina a cor de fundo como a cor desejada
-        self.label.place(x=240, y=48)  # Ajustar posição
+        self.label = Label(root, text=str(self.valor_inteiro), font=("ArchivoBlack", 38), fg="#FFFFFF", bg="#AA125F")
+        self.label.place(x=240, y=48)
+
+    def exibirImagem(self):
+        imagem = Image.open("assets/placeholder.png")
+        imagem_tk = ImageTk.PhotoImage(imagem)
+
+        self.background_label = Label(root, image=imagem_tk)
+        self.background_label.place(relwidth=1, relheight=1)
+        self.background_label.image = imagem_tk
 
     def atualizar_valor(self, novo_valor):
-        # Atualiza o valor inteiro e atualiza o texto na Label
         self.valor_inteiro = novo_valor
         self.label.config(text=str(self.valor_inteiro))
 
