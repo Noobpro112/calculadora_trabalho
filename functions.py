6cfc34aeed1f7d0a3a9c33be4bad940380c50ea3
Gustax0 <gustavohenriquegomesdias@gmail.com>
2023-09-27 12:31:30 -0300

    foda-se

---
+++ functions.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+from tkinter import *
+from PIL import Image, ImageTk
+
+class Botoes():
+    def __init__(self):
+        self.valor_inteiro = 0
+        self.teste(teste)
+
+    def teste(self, novo_valor):
+        self.valor_inteiro = novo_valor
+        print(novo_valor)
+        
+Botoes()
