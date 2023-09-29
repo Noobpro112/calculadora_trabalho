042748ba66d04b7662178fcc9b0e53a48f88b172
Gustax0 <gustavohenriquegomesdias@gmail.com>
2023-09-29 11:40:28 -0300

    Update

--- interfaceTeste.py
+++ interfaceTeste.py
@@ -5,25 +5,33 @@ from functions import Botoes
 
 root = tk.Tk()
 
+
 class Calculadora:
     def __init__(self):
         self.root = root
         self.botoes = Botoes()
         self.interface()
         self.exibirImagem()  # Primeiro exibir a imagem
-        self.exibirNum()     # Depois exibir o texto
-                            #SIM ISSO FOI IMPORTANTE,
+        self.exibirNum()  # Depois exibir o texto
+        # SIM ISSO FOI IMPORTANTE,
 
     def interface(self):
-        root.title('Calculadora')
-        root.geometry('320x480')
+        root.title("Calculadora")
+        root.geometry("320x480")
         root.resizable(False, False)
 
     def exibirNum(self):
         valor_default = self.botoes.get_string()
         num_frame = Frame(root, bg="#AA125F")
         num_frame.place(x=56, y=28, width=230, height=80)
-        num_label = Label(num_frame, text=valor_default, font=("ArchivoBlack", 28), fg="#FFFFFF", bg="#AA125F", anchor="w")
+        num_label = Label(
+            num_frame,
+            text=valor_default,
+            font=("ArchivoBlack", 28),
+            fg="#FFFFFF",
+            bg="#AA125F",
+            anchor="w",
+        )
         num_label.config(wraplength=230)
         num_label.pack(fill=BOTH, expand=YES)
 
@@ -39,6 +47,7 @@ class Calculadora:
     def atualizar_valor(self, novo_valor):
         self.botoes.valor_default = novo_valor
         self.exibirNum()
+
 
 Calculadora()
 root.mainloop()
